62f76e13fd1fefa2763319e007db50ea28a71734
Raffi Macaraig <108770810+DaRafster@users.noreply.github.com>
2023-01-23 18:19:01 -0800

    end of class

--- LinkedStack.h
+++ LinkedStack.h
@@ -19,17 +19,57 @@ class Stack {
 public:
 	Stack() {
 		size = 0;
-		s = nullptr;
+		top = nullptr;
+	}
+
+	Stack(const Stack<T>& obj) {
+		if (obj.top == nullptr)
+			top = nullptr;
 	}
 
 	~Stack() {
-		delete[] s;
+		while (top != nullptr) {
+			StackNode<T>* nextNode = top->next;
+			delete top;
+			top = nextNode;
+		}
 	}
 
 	void push(const T& item) {
 		StackNode<T>* nodeToPush = new StackNode(item);
 		nodeToPush->next = top;
 		top = nodeToPush;
+
+		size++;
+	}
+
+	T pop() {
+		if (isEmpty())
+			throw runtime_error("Stack Underflow!");
+
+		StackNode<T>* nodeToPop = top;
+		top = top->next;
+		T valueToPop = nodeToPop->value;
+		delete nodeToPop;
+
+		size--;
+
+		return valueToPop;
+	}
+
+	T peek() {
+		if (isEmpty())
+			throw runtime_error("Stack Underflow!");
+
+		return top->value;
+	}
+
+	bool isEmpty() {
+		return top == nullptr;
+	}
+
+	int getSize() {
+		return size;
 	}
 private:
 	StackNode<T>* top;
